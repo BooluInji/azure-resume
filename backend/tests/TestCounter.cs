55e0d23445a7689c46dd5d51adf05dbd8e1de961
Booluinji <booluinji@gmail.com>
2023-10-03 10:16:09 -0500

    testing

--- backend/tests/TestCounter.cs
+++ backend/tests/TestCounter.cs
@@ -17,15 +17,16 @@ namespace tests
         private readonly ILogger logger = TestFactory.CreateLogger();
 
         [Fact]
-        public async void Http_trigger_should_return_known_string()
+        public async Task Http_trigger_should_return_known_string()
         {
             var counter = new Company.Function.Counter();
             counter.Id = "1";
             counter.Count = 2;
             var request = TestFactory.CreateHttpRequest();
-            var response = (HttpResponseMessage) Company.Function.GetResumeCounter.Run(request, counter, out counter, logger);
-            Assert.Equal(3, counter.Count);
+            using (var response = (HttpResponseMessage) Company.Function.GetResumeCounter.Run(request, counter, out counter, logger))
+            {
+                Assert.Equal(3, counter.Count);
+            }
         }
-
     }
 }
