ff8d63acb777ef31b16e6064a3a8f54c97fb2acb
Booluinji <booluinji@gmail.com>
2023-10-06 16:39:47 -0500

    Refresh of main site

--- frontend/main.js
+++ frontend/main.js
@@ -2,19 +2,23 @@ window.addEventListener("DOMContentLoaded", (event) => {
   getVisitCount();
 });
 
-const functionApiUrl = "https://getresumecounter-inji.azurewebsites.net/api/GetResumeCounter?code=VqvXhRU2TzlGgyYmk8BAgH6TUOTYD2qT014wzVy9SZW9AzFuTcTnqQ=="
-const localfunctionApi = "";
+const functionApi =
+  "https://getresumecounter-inji.azurewebsites.net/api/GetResumeCounter?code=VqvXhRU2TzlGgyYmk8BAgH6TUOTYD2qT014wzVy9SZW9AzFuTcTnqQ==";
+const localfunctionApi = "http://localhost:7071/api/GetResumeCounter";
 
 const getVisitCount = () => {
   let count = 30;
-fetch(functionApiUrl).then(response => {
-  return response.json()
-}).then(response =>{
-  console.log("Website called function API.");
-  count = response.count;
-  document.getElementById("counter").innerHTML= count;
-}).catch(function(error){
-  console.log(error);
-});
-return count;
-}
+  fetch(functionApi)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log("Website called function API.");
+      count = response.count;
+      document.getElementById("counter").innerText = count;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return count;
+};
